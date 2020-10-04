533212c2ebc22f24352f4bf04760a7457e7af91e
Aditya-Amin <adittyaamin@gmail.com>
2020-10-04 17:06:17 +0600

    Updates

---
+++ precache-manifest.287fdc2ce9e6744828a72193a9614fbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f75d7d89deca1a71baf53a718d89a27",
+    "url": "/React-Test/index.html"
+  },
+  {
+    "revision": "fb660396651bce28805d",
+    "url": "/React-Test/static/css/main.28248cc6.chunk.css"
+  },
+  {
+    "revision": "795ee46bc80865bc6f13",
+    "url": "/React-Test/static/js/2.5b8c3db4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Test/static/js/2.5b8c3db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb660396651bce28805d",
+    "url": "/React-Test/static/js/main.2bd486cf.chunk.js"
+  },
+  {
+    "revision": "a02f0de2f9a5ffede858",
+    "url": "/React-Test/static/js/runtime-main.a956f1e3.js"
+  }
+]);
